3cf85ae83b7cd1428d08180759808d36b8017c15
asynkov <asynkov@belkacar.ru>
2024-08-04 19:34:31 +0300

    [hw1] unused imports removed

--- homework/homework-1-xml-context/src/main/java/otus/spring/dao/QuestionCsvDao.java
+++ homework/homework-1-xml-context/src/main/java/otus/spring/dao/QuestionCsvDao.java
@@ -11,8 +11,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Reader;
-import java.net.URISyntaxException;
-import java.nio.file.Files;
 import java.util.List;
 import java.util.Optional;
 
